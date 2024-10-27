c6224eb0963f740624b8e7da24d648e2b5af67aa
Owolon <keepoutmind@gmail.com>
2024-10-28 00:33:48 +0300

    Add files via upload

---
+++ Лаб7/Лаб7/Лаб7/Решение.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <math.h>
+#define _USE_MATH_DEFINES 
+#define CRT_SECURE_NO_DEPRECATE
+//0
+double formula(double X)
+{
+	double result;
+	result = (double)pow(X, 3) + 3.*(double)pow(X, 2) - 3.;
+	return result;
+}
+int main()
+{
+	setlocale(LC_CTYPE, "RUS");
+	int n;
+	int N = 0;
+	int Nb = 0;
+	printf("¬ведите значение n:");
+	scanf_s("%d", &n);
+	for (int st = 1; st <= n; st++) 
+	{
+		if (st == 1)
+		{
+			printf("+");
+		}
+		printf("*!");
+		N++;
+		if (N == n/2)
+		{
+			break;
+		}
+	}
+	for (int end = 1; end <= n; end++)
+	{
+		printf("=!");
+		Nb++;
+		if (Nb == n / 2)
+		{
+			break;
+		}
+	}
+//1
+	int ne, me;
+	int s = 0;
+	int k = 1;
+	printf("\n¬ведите значение m:");
+	scanf_s("%d", &me);
+	printf("\n¬ведите значение n:");
+	scanf_s("%d", &ne);
+
+	for (int i=ne; i>=me;i--)
+	{
+		s=s+i;
+		printf("\n¬ыполнено %d раз", k++);
+		printf("\n–езультат %d", s);
+	}
+//1A
+	int M;
+
+	printf("\n¬ведите значение степени:\n");
+	scanf_s("%d", &M);
+	for (int i = 1;i<=M;i++)
+	{
+		if (i >= 10)
+		{
+			printf("\n2^%d = %20.1lf K", i, (pow(2, i)) /  1024);
+		}
+			if (i < 10)
+			{
+				printf("\n2^%d = %20.1lf", i, (pow(2, i)));
+			}
+	}
+//2
+	double X;
+	double Xc;
+	double h;
+	double Y;
+	printf("\n‘ункци€ f(x) = |e^w - 2| - x^2\n");
+	printf("\n¬ведите начало интервала:\n");
+	scanf_s("%lf", &X);
+	printf("\n¬ведите конец интервала:\n");
+	scanf_s("%lf", &Xc);
+	printf("\n¬ведите шаг табул€ции:\n");
+	scanf_s("%lf", &h);
+
+	printf("_____________\n");
+	for (;X <= Xc; X+=h)
+	{
+		Y = formula(X);
+		printf("|%3.3lf|%3.3lf|\n", X,Y);
+	}
+	printf("-------------\n");
+//3
+	int Nv;
+	float q = 1;
+	printf("¬ведите значение N:\n");
+	scanf_s("%d", &Nv);
+	for (int l=2;l<=Nv;l++)
+	{
+		q = q + (1./l);
+		printf("\n–езультат: %.2lf", q);
+	}
+
+}	
